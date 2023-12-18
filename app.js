8a5608c40e60f2bbf8df80b3c5d66f63e7f3161f
Lucy Lole <lucy.lole@aurum.solutions>
2023-12-18 17:12:58 +0000

    Day 17 half but broken :(

--- app.js
+++ app.js
@@ -1,4 +1,94 @@
+"use strict";
 let fs = require('fs');
-let lines = fs.readFileSync("./Inputs/input17.txt").toString().split('\n');
+let grid = fs.readFileSync("./Inputs/day17example.txt").toString().split('\n').map(x => x.split(''));
+let horLength = grid[0].length;
+let verLength = grid.length;
+let directions = {left:0,up:1,right:2,down:3};
+let finalSet = [];
+let cameFrom = {};
+let nodes = {};
+for (let y=0;y<grid.length;y++) {
+    for(let x=0;x<grid[y].length; x++) {
+        nodes[y + '|' + x] = {y:y, x:x, weight:parseInt(grid[y][x]), gScore:99999999999999, fScore:99999999999999};
+    }
+}
 
-console.log();
+// Construct our graph
+Object.values(nodes).forEach(node => {
+    node.neighbors = [];
+    if (node.x > 0)           node.neighbors.push(nodes[node.y + '|' + (node.x-1)]);
+    if (node.x < horLength-1) node.neighbors.push(nodes[node.y + '|' + (node.x+1)]);
+    if (node.y > 0)           node.neighbors.push(nodes[(node.y-1) + '|' + node.x]);
+    if (node.y < verLength-1) node.neighbors.push(nodes[(node.y+1) + '|' + node.x]);
+})
+
+// Set up for A*
+function h(node) {
+    return (verLength - node.y) + (horLength - node.x);
+}
+
+function getKey(node) {
+    return node.y.toString() + '|' + node.x.toString();
+}
+
+function reconstructPath(current) {
+    let totalWeight = 0;
+
+
+    while (current != start) {
+        finalSet.push(current);
+        totalWeight += current.weight;
+        current = cameFrom[getKey(current)].node;
+    }
+
+    finalSet.push(current);
+    finalSet.reverse();
+    return totalWeight;
+}
+
+let start = nodes["0|0"];
+let end = nodes[(verLength-1) + '|' + (horLength-1)];
+
+start.gScore = 0;
+start.fscore = h(start);
+
+let openSet = [start];
+
+let total = 0;
+
+// Do A*
+while (openSet.length > 0) {
+    let current = openSet.shift();
+
+    if (current == end) {
+        total = reconstructPath(current);
+        break;
+    }
+
+    current.neighbors.forEach(neighbor => {
+        let tentGScore = (current.gScore + neighbor.weight);
+        if (tentGScore < neighbor.gScore) {
+            let tmpDir;
+            let tmpStep = 1;
+
+            if (neighbor.x > current.x) tmpDir = directions.right;
+            else if (neighbor.x < current.x) tmpDir = directions.left;
+            else if (neighbor.y > current.y) tmpDir = directions.down;
+            else if (neighbor.y < current.y) tmpDir = directions.up;
+
+            if (cameFrom[getKey(current)] != undefined && cameFrom[getKey(current)].dir == tmpDir) tmpStep = (cameFrom[getKey(current)].step + 1);
+
+            if (tmpStep > 3) return;
+
+            cameFrom[getKey(neighbor)] = {node:current,dir:tmpDir,step:tmpStep};
+            neighbor.gScore = tentGScore;
+            neighbor.fScore = tentGScore + h(neighbor);
+            if (openSet.find(x => x == neighbor) == undefined) openSet.push(neighbor);
+        }
+
+    });
+
+    openSet = openSet.sort((a,b) => a.fScore - b.fScore);
+}
+
+console.log(total);
